90adfbfe0e43e7029983a8463bd84d2db2be12dd
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2020-12-09 01:36:56 -0300

    Creates Student Entity with unit tests.

---
+++ src/main/java/school/Student.java
@@ -0,0 +1,41 @@
+package school;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Student {
+
+    private final CPF cpf;
+    private final String name;
+    private final Email email;
+
+    private final List<PhoneNumber> phoneNumbers;
+
+    public Student(CPF cpf, String name, Email email) {
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        phoneNumbers = new ArrayList<>();
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public List<PhoneNumber> getPhoneNumbers() {
+        return Collections.unmodifiableList(phoneNumbers);
+    }
+
+    public void addsPhoneNumber(String areaCode, String lineNumber) {
+        phoneNumbers.add(new PhoneNumber(areaCode, lineNumber));
+    }
+}
---
+++ src/test/java/school/StudentTest.java
@@ -0,0 +1,26 @@
+package school;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StudentTest {
+
+    @Test
+    void addsPhoneNumber() {
+
+        String areaCode = "11";
+        String lineNumber = "98765432";
+
+        Student student = StudentBuilder.withNameCPFEmail("Ricardo", "12345678900", "ricardo@email.com").build();
+
+        assertEquals(0, student.getPhoneNumbers().size());
+
+        student.addsPhoneNumber(areaCode, lineNumber);
+
+        assertEquals(1, student.getPhoneNumbers().size());
+        assertEquals(areaCode, student.getPhoneNumbers().get(0).getAreaCode());
+        assertEquals(lineNumber, student.getPhoneNumbers().get(0).getLineNumber());
+    }
+
+}
